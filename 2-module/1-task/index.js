b0c8a3a0409d605e77b904aeb55cf1a7c223b521
loramisautova <misautova.lora@gmail.com>
2023-11-14 18:04:00 +0300

    add 2-module all task solutions

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   let sum = 0;
 
   for (let key in salaries) {
-    if (Boolean(salaries[key]) && typeof salaries[key] === 'number' && isFinite(salaries[key])) {
+    if (salaries[key] && typeof salaries[key] === 'number' && isFinite(salaries[key])) {
       sum += salaries[key];
     }
   }
